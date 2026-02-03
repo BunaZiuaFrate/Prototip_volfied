21f0f99316aa36fc613da37a34c79fac13cf9882
gabi <gabicojocaru25@de-github.com>
2026-02-03 11:19:12 +0200

    Fix build errors

--- EntityFactory.cpp
+++ EntityFactory.cpp
@@ -2,21 +2,20 @@
 #include "Player.h"
 #include "Enemy.h"
 #include "Obstacle.h"
-#include "Utils.h" // Pentru GameConfig
-#include <stdexcept> // Pentru std::invalid_argument
+#include "Utils.h"
+#include <stdexcept>
 
 std::shared_ptr<Entity> EntityFactory::createEntity(Type type, int x, int y) {
     switch (type) {
         case Type::Player:
             return std::make_shared<Player>("Hero", x, y);
         case Type::EnemySparx:
-            // Folosim GameConfig pentru dificultate
             return std::make_shared<Enemy>("Sparx", x, y, 10 * GameConfig::getInstance().getDifficulty());
         case Type::EnemyQix:
             return std::make_shared<Enemy>("Qix", x, y, 25 * GameConfig::getInstance().getDifficulty());
         case Type::Obstacle:
             return std::make_shared<Obstacle>(x, y, false);
         default:
-            throw std::invalid_argument("Tip de entitate necunoscut!");
+            throw std::invalid_argument("Tip necunoscut!");
     }
 }
